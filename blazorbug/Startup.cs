335da5dec286e6b5deab14d74011816885eee1ef
Leonardo Costa <pctech.leo@gmail.com>
2021-08-16 21:50:18 -0400

    Initial commit

---
+++ blazorbug/Startup.cs
@@ -0,0 +1,45 @@
+using blazorbug.Data;
+
+namespace blazorbug
+{
+    public class Startup
+    {
+        IConfiguration _config;
+
+        public Startup(IConfiguration configuration)
+            => _config = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
